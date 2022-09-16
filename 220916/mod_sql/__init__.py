f4f5e0c4d8d8d2dc7c153b3cfefac40693f62015
bsmoon-86 <ekflafg@naver.com>
2022-09-16 17:33:36 +0900

    220916_update

---
+++ 220916/mod_sql/__init__.py
@@ -0,0 +1,28 @@
+import pymysql
+import pandas as pd
+
+class Database():
+    def __init__(self):
+        self._db = pymysql.connect(
+            host = "localhost", 
+            user = "root", 
+            passwd= "1234", 
+            db = "ubion", 
+            port = 3306
+        )
+        self.cursor = self._db.cursor(pymysql.cursors.DictCursor)
+
+    def execute(self, sql, values=[]):
+        self.cursor.execute(sql, values)
+
+    def commit(self):
+        self._db.commit()
+
+    def executeall(self, sql, values=[]):
+        self.cursor.execute(sql, values)
+        self.result = self.cursor.fetchall()
+        return pd.DataFrame(self.result)
+    
+    def close(self):
+        self._db.close()
+    
